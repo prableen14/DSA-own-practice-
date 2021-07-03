be7653d152889eef131496da5ab17e96b11208e5
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-07-03 12:05:40 +0530

    Create 3Sum.cpp

---
+++ 3Sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+       int n = nums.size();
+
+        if(n < 3)
+            return {};
+        vector<vector<int>> res; 
+        sort(nums.begin() , nums.end());
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int j = i+1, k = n-1;
+            
+            while(j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0)
+                    j++;
+                else if(sum > 0)
+                    k--;
+                else
+                {
+                    res.push_back({nums[i] , nums[j] , nums[k]});
+                    
+                    while(j < k && nums[j] == nums[j+1]) j++;
+                    while(j < k && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
+                }
+            }
+                
+        }
+        return res;   
+    }
+};
