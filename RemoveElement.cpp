00c94732953406c9176ee443fefe57eb8a5637ab
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-07-03 13:08:59 +0530

    Update RemoveElement.cpp

--- RemoveElement.cpp
+++ RemoveElement.cpp
@@ -12,3 +12,18 @@ index+=1;
 return index;
     }
 };
+//OR
+class Solution {
+public int removeElement(vector<int>& nums, int val) {
+    if(nums.size()==0) return 0;
+    int valid_size=0;
+    for(int i =0;i<nums.size();i++)
+  {
+    if(nums[i] != val){
+        nums[valid_size]=nums[i];
+        valid_size++;
+    }
+}
+    return valid_size;
+}
+}
