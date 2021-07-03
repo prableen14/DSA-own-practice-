6b72b2e4852bcaa54af872924735d0ba70db5139
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-07-03 12:20:41 +0530

    Create RemoveDuplicates.cpp

---
+++ RemoveDuplicates.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    int n = nums.size();
+    if(n == 0 || n == 1){
+        return n;
+    }
+    
+    int j=0;
+    
+    for(int i = 0; i < n-1; i++){
+        
+        if(nums[i] != nums[i+1]){
+           nums[j++] = nums[i];
+        }
+    }
+    nums[j++] = nums[n-1];
+    return j;
+
+    }
+};
