eb42d57f7a731b4848e2a44cc0e75d5824a03540
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-07-08 15:20:54 +0530

    Create AddFirst.java

---
+++ Foundation/Linked List/AddFirst.java
@@ -0,0 +1,15 @@
+    public void addFirst(int val) {
+      // write your code here
+      Node temp= new Node();
+      temp.data=val;
+      if(size==0){
+      head=tail=temp;
+      temp.next=null;
+      size++;
+      }
+      else{
+      temp.next=head;
+      head=temp;
+      size++;
+    }
+    }
