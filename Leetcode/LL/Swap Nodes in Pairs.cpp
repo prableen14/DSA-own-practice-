fabadb593eae354bb73d47f4f421bc706c2e8727
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-09-13 08:09:00 +0530

    Update and rename Swap Nodes in Pairs.java to Swap Nodes in Pairs.cpp

--- Leetcode/LL/Swap Nodes in Pairs.cpp
+++ Leetcode/LL/Swap Nodes in Pairs.cpp
@@ -2,6 +2,31 @@
 You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.) */
 
 class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next=head;
+         ListNode * runner = dummy;
+        
+        while(runner->next!=NULL && runner->next->next!=NULL){
+            ListNode* r1= runner->next;
+             ListNode* r2= runner->next->next;
+            
+            runner->next=r2;
+            r1->next=r2->next;
+            r2->next=r1;
+            
+            //Move to the next pair
+            runner= runner->next->next;
+            }
+        return dummy->next;
+        }
+    
+};
+
+//JAVA SOLUTION
+
+class Solution {
     public ListNode swapPairs(ListNode head) {
         ListNode dummy= new ListNode(0);
         dummy.next=head;
