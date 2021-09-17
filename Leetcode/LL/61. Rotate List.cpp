39589d469fe1c7f715a747bfc7757b1dff5694f4
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-09-18 03:47:10 +0530

    Update 61. Rotate List.cpp

--- Leetcode/LL/61. Rotate List.cpp
+++ Leetcode/LL/61. Rotate List.cpp
@@ -6,6 +6,12 @@ Output: [4,5,1,2,3]
 Example 2:
 Input: head = [0,1,2], k = 4
 Output: [2,0,1]
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
 */
 
 
