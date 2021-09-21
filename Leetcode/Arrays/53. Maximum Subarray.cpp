2389f2e619dcc80005ec7d1d5d55458b1179c4b8
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-09-21 13:46:18 +0530

    Create 53. Maximum Subarray.cpp

---
+++ Leetcode/Arrays/53. Maximum Subarray.cpp
@@ -0,0 +1,31 @@
+/*Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+
+Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Example 2:
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23 */
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+         int n = nums.size();
+        int max_so_far = INT_MIN;
+        int curr_max = 0;
+        for(int i = 0;i < n;i++){
+            curr_max += nums[i];
+            if(max_so_far < curr_max) max_so_far = curr_max;
+            if(curr_max < 0) curr_max = 0;
+        }
+        return max_so_far;
+    }
+};
