1467bcf3861a7e69a5e3393f6e5715e9e5d150b9
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-09-24 08:22:23 +0530

    Update Pattern 7.cpp

--- Foundation/Pattern/Pattern 7.cpp
+++ Foundation/Pattern/Pattern 7.cpp
@@ -22,3 +22,29 @@ int main(int argc, char **argv){
     cout<<"\n";
 }
 }
+
+/*
+10000
+02000
+00300
+00040
+00005
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        if(i==j)
+        cout<<i;
+        else
+        cout<<"0";
+    }
+    cout<<"\n";
+}
+}
