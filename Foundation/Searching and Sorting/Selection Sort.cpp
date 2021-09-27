00fdf79d2c3ef3c2519875bb4e5f0065e4c7849b
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-09-27 08:29:52 +0530

    Create Selection Sort.cpp

---
+++ Foundation/Searching and Sorting/Selection Sort.cpp
@@ -0,0 +1,3 @@
+/* TC: O(n^2) in all cases
+Selection sort can be good at checking if everything is already sorted. It is also good to use when memory space is limited. 
+This is because unlike other sorting algorithms, selection sort doesn't go around swapping things until the very end, resulting in less temporary storage space used.
