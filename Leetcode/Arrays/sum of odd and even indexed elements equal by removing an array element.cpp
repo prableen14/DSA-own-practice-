9543f1939257424ab201a75c8d109857524804fa
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-10-01 22:46:43 +0530

    Update sum of odd and even indexed elements equal by removing an array element.cpp

--- Leetcode/Arrays/sum of odd and even indexed elements equal by removing an array element.cpp
+++ Leetcode/Arrays/sum of odd and even indexed elements equal by removing an array element.cpp
@@ -1,4 +1,16 @@
 //Count ways to make sum of odd and even indexed elements equal by removing an array element
+
+/*Naive Approach: The simplest approach to solve this problem is to traverse the array and for each array element, check if removing the element from the array makes the sum of even-indexed and odd-indexed array elements equal or not. If found to be true, then increment the count. Finally, print the count.
+TC: O(N^2) 
+Optimized Approach: TC:O(N)
+Removing any element from the given array makes even indices of succeeding elements as odd and odd indices of the succeeding elements as even. 
+Algorithm:
+1.Initialize two variables, say evenSum and oddSum, to store the sum of odd-indexed and even-indexed elements of the given array respectively.
+2.Traverse the array using variable i.
+3.Remove every ith element of the array and update the sum of the remaining even-indexed elements and the odd-indexed elements based on the above observation. Check if the sums are equal or not. If found to be true, then increment the count.
+4.Finally, print the count obtained.
+  */  
+
 #include <iostream>
 
 using namespace std;
