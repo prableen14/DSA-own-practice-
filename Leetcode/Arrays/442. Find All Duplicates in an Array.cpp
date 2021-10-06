b67a8b6f06d152f9e7b2d46f53848a6b88867c48
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-10-06 22:52:35 +0530

    Update 442. Find All Duplicates in an Array.cpp

--- Leetcode/Arrays/442. Find All Duplicates in an Array.cpp
+++ Leetcode/Arrays/442. Find All Duplicates in an Array.cpp
@@ -11,7 +11,14 @@ Output: [1]
 
 Example 3:
 Input: nums = [1]
-Output: []*/
+Output: []
+
+1) Find index and mark its presence.
+2) If already marked( which means if its value is -ve) ,that means duplicacy encoutered. Add that duplicate element in the answer.
+For Example, an array is [4,3,2,7,8,2,3,1], then when loop starts then 1st element (i.e 4) will go to 4th positiin or 3rd index (nums[i]-1). 
+After that, to mark the presence of that index, change the value present at that index to its negative, 
+so that when duplicate element is encoutered we can check if that value is negative or positve. if its already negative then that means duplicacy happened. Add that duplicate element in the answer by adding 1 to it.
+*/
 
 class Solution {
 public:
