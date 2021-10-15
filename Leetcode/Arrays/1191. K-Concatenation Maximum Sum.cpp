b1e5f54f4c05d33f6f7abca0f3b9f933607fa213
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-10-15 19:38:06 +0530

    Update 1191. K-Concatenation Maximum Sum.cpp

--- Leetcode/Arrays/1191. K-Concatenation Maximum Sum.cpp
+++ Leetcode/Arrays/1191. K-Concatenation Maximum Sum.cpp
@@ -2,34 +2,35 @@
 1191. K-Concatenation Maximum Sum
 
 Given an integer array arr and an integer k, modify the array by repeating it k times.
-
 For example, if arr = [1, 2] and k = 3 then the modified array will be [1, 2, 1, 2, 1, 2].
-
 Return the maximum sub-array sum in the modified array. Note that the length of the sub-array can be 0 and its sum in that case is 0.
-
 As the answer can be very large, return the answer modulo 109 + 7.
 
- 
-
 Example 1:
-
 Input: arr = [1,2], k = 3
 Output: 9
-Example 2:
 
+Example 2:
 Input: arr = [1,-2,1], k = 5
 Output: 2
-Example 3:
 
+Example 3:
 Input: arr = [-1,-2], k = 7
 Output: 0
  
 
 Constraints:
-
 1 <= arr.length <= 105
 1 <= k <= 105
 -104 <= arr[i] <= 104
+
+Solution:
+There are 3 cases : 
+1. K=1, simply do kadane traversal
+2. if sum of given array is negative, then answer can obtained from first two copies of array.
+3. if sum>0, then we can do merge first and last copy of array(which is same as merging first two copies) , do kadane and  
+    add remaining between sum of arrays/copies of array i.e. (k-2)*sum
+    
 */
 
 class Solution {
