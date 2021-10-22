66d8e8fd5ed39a6d5cab827103e8623615a27496
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-10-23 02:15:50 +0530

    Update and rename 92. Reverse Linked List II.java to 92. Reverse Linked List II.cpp

--- Leetcode/LL/92. Reverse Linked List II.cpp
+++ Leetcode/LL/92. Reverse Linked List II.cpp
@@ -20,38 +20,39 @@ Output: [5]*/
  * }
  */
 class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-            if(head==null || head.next==null){
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+         if(head==NULL || head->next==NULL){
       return head;
     }
 
-    ListNode dummy = new ListNode(-1);
-    dummy.next = head;
+    ListNode* dummy = new ListNode(-1);
+    dummy->next = head;
     // till the left index
-    ListNode it = dummy;
-    ListNode prevIt = null;
+    ListNode* it = dummy;
+    ListNode* prevIt = NULL;
     for(int i=0;i<left;i++){
       prevIt = it;
-      it = it.next;
+      it = it->next;
     }
 
     //reverse
-    ListNode itR = it;
-    ListNode prevR = prevIt;
+    ListNode* itR = it;
+    ListNode* prevR = prevIt;
 
     // reversing a linked list
     for(int i=left;i<=right;i++){
-      ListNode forward = itR.next;
-      itR.next = prevR;
+      ListNode* forward = itR->next;
+      itR->next = prevR;
       prevR = itR;
       itR = forward;
     }
 
     //connect
-    prevIt.next = prevR;
-    it.next = itR;
+    prevIt->next = prevR;
+    it->next = itR;
 
-    return dummy.next;
-
+    return dummy->next;
+   
     }
-}
+};
