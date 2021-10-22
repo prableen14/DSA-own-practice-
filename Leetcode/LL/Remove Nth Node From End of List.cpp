47094b6df2cbf442809d15e4e5989bd915bb5458
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-10-22 22:51:33 +0530

    Update Remove Nth Node From End of List.cpp

--- Leetcode/LL/Remove Nth Node From End of List.cpp
+++ Leetcode/LL/Remove Nth Node From End of List.cpp
@@ -15,39 +15,6 @@ Output: [1] */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(!head) return head;
-        ListNode* dummy = new ListNode(0);
-        dummy -> next = head;
-        ListNode * p = dummy, *c = head, *temp=NULL;
-        int m= n;
-        while(c && m!=1){
-            c=c->next;
-            m--;
-        }
-        temp = head;
-        
-        //Now temp stands on node which is to be deleted
-        while(c->next){
-            c=c->next;
-            temp=temp->next;
-            p=p->next;
-        }
-        
-        ListNode * nex = NULL;
-        
-        if(temp->next) nex= temp -> next;
-        else nex = NULL;
-        
-        p->next = nex;
-        return dummy->next;
-    
-    }
-};
-
-// ANOTHER SOLUTION
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* dummy= new ListNode(0);
 dummy->next=head;
 
