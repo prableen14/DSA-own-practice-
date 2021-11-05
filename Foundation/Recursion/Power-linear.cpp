38aae684ed019749e0622f615803239638f06b0d
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-05 13:20:15 +0530

    Update Power-linear.cpp

--- Foundation/Recursion/Power-linear.cpp
+++ Foundation/Recursion/Power-linear.cpp
@@ -3,7 +3,7 @@
 2. You are given another number n.
 3. You are required to calculate x raised to the power n. Don't change the signature of power function.
 
-Note1 -> The previous version expects the call stack to be of n height. This function expects call function to be only log(n) high.
+Note1 -> This version expects the call stack to be of n height.
 
 Note2 -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
 Input Format
@@ -25,16 +25,16 @@ Sample Output
 #include<iostream>
 using namespace std;
 
-int powerLogarithmic(int x,int n){
+int powerLinear(int x,int n){
     // write your code here
     if(n==0) return 1;
     
-    int xnm1= powerLogarithmic(x,n-1);
+    int xnm1= powerLinear(x,n-1);
     int xn= x*xnm1;
     return xn;
 }
 
 int main(){
     int x,n; cin>>x>>n;
-    cout<<powerLogarithmic(x,n);
+    cout<<powerLinear(x,n);
 }
