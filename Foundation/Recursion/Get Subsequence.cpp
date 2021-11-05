e2a5aba37521e03766e26fcade42e452c1224f6c
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-05 18:16:30 +0530

    Create Get Subsequence.cpp

---
+++ Foundation/Recursion/Get Subsequence.cpp
@@ -0,0 +1,6 @@
+/*Subsequence - 2^no.of characters
+For eg: Given abc then 2^3=8 possible sub sequence (2 bcoz aayega ya nhi aayega character)
+
+SubString: No. of possible substrings for n length of string are: n*(n+1)/2
+
+*/
