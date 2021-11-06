dace28accf10e1af3f3430e02f90a569fb52f54c
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-06 23:26:44 +0530

    Update Get Subsequence.cpp

--- Foundation/Recursion/Get Subsequence.cpp
+++ Foundation/Recursion/Get Subsequence.cpp
@@ -4,3 +4,59 @@ For eg: Given abc then 2^3=8 possible sub sequence (2 bcoz aayega ya nhi aayega 
 SubString: No. of possible substrings for n length of string are: n*(n+1)/2
 
 */
+/*
+1. You are given a string str.
+2. Complete the body of getSS function - without changing signature - to calculate all subsequences of str.
+Use sample input and output to take idea about subsequences.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is.
+Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+A string str
+Output Format
+Contents of the arraylist containing subsequences as shown in sample output
+Sample Input
+abc
+Sample Output
+[, c, b, bc, a, ac, ab, abc]
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> gss(string s){
+    // write your code here
+    if(s.size()==0)
+    {vector<string> bres;
+    bres.push_back("");
+    return bres;}
+    char ch=s.at(0);
+    string ros=s.substr(1);
+    vector<string> rres= gss(ros);
+    
+    vector<string> mres;
+    for(string rstr: rres)
+    mres.push_back(""+rstr);
+    for(string rstr: rres)
+    mres.push_back(ch+rstr);
+    return mres;
+}
+
+int main(){
+    string s;
+    cin >> s;
+    vector<string> ans = gss(s);
+    int cnt = 0;
+
+    cout << '[';
+    for (string str : ans){
+        if (cnt != ans.size() - 1)
+            cout << str << ", ";
+        else
+            cout << str;
+        cnt++;
+    }
+    cout << ']';
+}
