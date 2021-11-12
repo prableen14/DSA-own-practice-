f5b44aba45903107588a7b25b54154b3b7aaf993
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-12 23:02:00 +0530

    Create Get Maze Path.cpp

---
+++ Foundation/Recursion/Get Maze Path.cpp
@@ -0,0 +1,20 @@
+/*1. You are given a number n and a number m representing number of rows and columns in a maze.
+2. You are standing in the top-left corner and have to reach the bottom-right corner. Only two moves are allowed 'h' (1-step horizontal) and 'v' (1-step vertical).
+3. Complete the body of getMazePath function - without changing signature - to get the list of all paths that can be used to move from top-left to bottom-right.
+Use sample input and output to take idea about output.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+A number n
+A number m
+Output Format
+Contents of the arraylist containing paths as shown in sample output
+Constraints
+0 <= n <= 10
+0 <= m <= 10
+Sample Input
+3
+3
+Sample Output
+[hhvv, hvhv, hvvh, vhhv, vhvh, vvhh]
+*/
