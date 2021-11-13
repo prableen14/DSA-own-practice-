a860723bfd7a98ecb49cecda03b1c81cd1a96ab9
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-13 17:27:44 +0530

    Update Get Maze Path.cpp

--- Foundation/Recursion/Get Maze Path.cpp
+++ Foundation/Recursion/Get Maze Path.cpp
@@ -18,3 +18,51 @@ Sample Input
 Sample Output
 [hhvv, hvhv, hvvh, vhhv, vhvh, vvhh]
 */
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// sr - source row
+// sc - source column
+// dr - destination row
+// dc - destination column
+vector <string> getMazePaths(int sr, int sc, int dr, int dc) {
+if(sr==dr && sc==dc) 
+{vector<string> bres;
+    bres.push_back("");
+    return bres;}
+    vector<string> hpaths;
+    vector<string> vpaths;
+    
+    if(sc<dc)
+    hpaths= getMazePaths(sr,sc+1,dr,dc);
+     if(sr<dr)
+    vpaths= getMazePaths(sr+1,sc,dr,dc);
+     vector<string> paths;
+     for(string hpath: hpaths)
+    paths.push_back("h"+ hpath);
+      for(string vpath: vpaths)
+    paths.push_back("v"+ vpath);
+    return paths;
+
+}
+
+void display(vector<string>& arr){
+    cout << "[";
+    for(int i = 0;i < arr.size();i++){
+        cout << arr[i];
+        if(i < arr.size() -1) cout << ", ";
+    }
+    
+    cout << "]"<<endl;
+}
+
+
+int main() {
+    int n,m; cin >> n >> m;
+    vector<string> ans = getMazePaths(0,0,n-1,m-1);
+    display(ans);
+
+    return 0;
+}
