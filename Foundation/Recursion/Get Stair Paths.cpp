5ace1cb65751ed791df2b594417b20803c768cca
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-13 17:16:20 +0530

    Update Get Stair Paths.cpp

--- Foundation/Recursion/Get Stair Paths.cpp
+++ Foundation/Recursion/Get Stair Paths.cpp
@@ -17,3 +17,48 @@ Sample Output
 [111, 12, 21, 3]
 
 */
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+vector<string> get_stair_paths(int n) {
+  //Write your code here
+  if(n==0)
+  {  vector<string> bres;
+    bres.push_back("");
+    return bres;}
+    else if(n<0){
+        vector<string> bres;
+    return bres;
+    }
+    vector<string> paths1=  get_stair_paths(n-1);
+    vector<string> paths2=  get_stair_paths(n-2);
+    vector<string> paths3=  get_stair_paths(n-3);
+    vector<string> paths;
+     for(string path: paths1)
+    paths.push_back("1"+ path);
+     for(string path: paths2)
+    paths.push_back("2"+ path);
+    for(string path: paths3)
+    paths.push_back("3"+ path);
+    
+    return paths;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> ans = get_stair_paths(n);
+  int cnt = 0;
+
+  cout << '[';
+  for (string str : ans) {
+    if (cnt != ans.size() - 1)
+      cout << str << ", ";
+    else
+      cout << str;
+    cnt++;
+  }
+  cout << ']';
+}
