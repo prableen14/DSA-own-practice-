cbf64f3b85df10384862d39a07813cb9b23a2505
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-17 23:03:14 +0530

    Update Flood Fill.cpp

--- Foundation/Recursion/Flood Fill.cpp
+++ Foundation/Recursion/Flood Fill.cpp
@@ -32,3 +32,36 @@ Sample Input
 Sample Output
 rddr
 */
+#include<iostream>
+
+#include<vector>
+
+using namespace std;
+// asf -> answer so far
+void floodfill(vector<vector<int>> maze, int sr, int sc, string asf, boolean[][] visited) {
+//write your code here
+if(sr<0 || sc<0 || sr==maze.size() || sc==maze[0].size() || maze[sr][sc]==1 || visited[sr][sc]==true)
+return;
+if(sr==maze.size()-1 || sc==maze[0].size()-1){
+    cout<<asf<<"\n";
+    return;
+}
+visited[sr][sc]=true;
+floodfill(maze,sr-1,sc,asf+"t", visited);
+floodfill(maze,sr,sc-1,asf+"l", visited);
+floodfill(maze,sr+1,sc,asf+"d", visited);
+floodfill(maze,sr,sc+1,asf+"r", visited);
+visited[sr][sc]=false;
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector < vector < int >> arr(n, vector < int > (m));
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      cin >> arr[i][j];
+
+  floodfill(arr, 0, 0, "");
+}
