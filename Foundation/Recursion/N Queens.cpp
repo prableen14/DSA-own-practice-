dc92dc9ea3e82728d5d5306fed5e2c81b816ab7f
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-18 02:20:59 +0530

    Update N Queens.cpp

--- Foundation/Recursion/N Queens.cpp
+++ Foundation/Recursion/N Queens.cpp
@@ -18,3 +18,39 @@ Sample Output
 0-1, 1-3, 2-0, 3-2, .
 0-2, 1-0, 2-3, 3-1, .
 */
+#include<bits/stdc++.h>
+using namespace std;
+bool isitasafeplaceforqueen(vector<vector<int>> chess, int row, int col){
+    for(int i=row-1,j=col;i>=0;i--){
+    if(chess[i][j]==1) return false;
+    }
+     for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+    if(chess[i][j]==1) return false;
+     }
+          for(int i=row-1,j=col+1;i>=0 && j<chess.size();i--,j++){
+    if(chess[i][j]==1) return false;
+     }
+     return true;
+}
+void printNQueens(vector<vector<int>> chess, string qsf, int row) {
+  //write your code here
+  if(row==chess.size()) {
+      cout<<qsf<<"."<<endl;
+  return;}
+  for(int col=0; col<chess.size();col++){
+      if(isitasafeplaceforqueen(chess,row,col)==true){
+          chess[row][col]=1;
+          printNQueens(chess,qsf+ to_string(row)+"-"+to_string(col)+", ",row+1) ;
+          chess[row][col]=0;
+      }
+  }
+
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<vector<int>> chess(n, vector<int> (n));
+
+  printNQueens(chess, "", 0);
+
+}
