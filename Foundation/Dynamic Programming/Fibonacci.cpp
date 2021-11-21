4020cd8f0745ce47b5c2039ca8315ad744895fc4
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-11-22 04:08:11 +0530

    Update Fibonacci.cpp

--- Foundation/Dynamic Programming/Fibonacci.cpp
+++ Foundation/Dynamic Programming/Fibonacci.cpp
@@ -1,1 +1,23 @@
+/*
+1. You are given a number n.
+2. You are required to print the nth element of fibonnaci sequence.
 
+Note -> Notice precisely how we have defined the fibonnaci sequence
+0th element -> 0
+1st element -> 1
+2nd element -> 1
+3rd element -> 2
+4th element -> 3
+5th element -> 5
+6th element -> 8
+Input Format
+A number n
+Output Format
+A number representing the nth element of fibonnaci sequence
+Constraints
+0 <= n <= 45
+Sample Input
+10
+Sample Output
+55
+*/
