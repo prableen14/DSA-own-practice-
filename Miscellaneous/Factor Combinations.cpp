793a8216f6baaf2f163477fc04085a19a5600f9e
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-25 05:07:59 +0530

    Create Factor Combinations.cpp

---
+++ Miscellaneous/Factor Combinations.cpp
@@ -0,0 +1,25 @@
+/*
+1. You are given a positive number n
+2. You need return all different combinations of its factors
+3. Input and output is handled for you
+4. It is a functional problem ,please do not modify main()
+ 
+NOTE: Factors must be greater than 1 and less than n
+For exapmple:
+Number: 8
+Factor combinations: 
+2,2,4
+4 2
+Input Format
+Input is handled for you
+Output Format
+Output is handled for you
+Sample Input                                        
+12                                   
+Sample Output                                       
+[2, 2, 3]
+[2, 6]
+[3, 4]
+*/
+
+//BACKTRACKING QUESTION
