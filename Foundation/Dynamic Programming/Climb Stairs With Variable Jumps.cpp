c4d38abd1fc6a72f093cc182debbdc5c9bf93822
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-26 21:23:58 +0530

    Update Climb Stairs With Variable Jumps.cpp

--- Foundation/Dynamic Programming/Climb Stairs With Variable Jumps.cpp
+++ Foundation/Dynamic Programming/Climb Stairs With Variable Jumps.cpp
@@ -25,3 +25,33 @@ Sample Input
 Sample Output
 5
 */
+#include <iostream>
+using namespace std;
+
+int cs(int arr[], int n){
+    int dp[n + 1];
+    for(int i = 0 ; i <= n; i++){
+        dp[i] = 0;
+    }
+    dp[n] = 1;
+
+    for(int i = n - 1; i >= 0; i--){
+        for(int jumps = 1; jumps <= arr[i]; jumps++){
+            if(i + jumps <= n){
+                dp[i] += dp[i + jumps];
+            }
+        }
+    }
+    return dp[0];
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i < n ;i++){
+        cin>>arr[i];
+    }
+    cout<<cs(arr,n)<<endl;
+}
