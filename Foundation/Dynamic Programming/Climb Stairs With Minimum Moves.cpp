38393f8296aa2f6cc827118070c55787a356e284
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-26 21:47:08 +0530

    Create Climb Stairs With Minimum Moves.cpp

---
+++ Foundation/Dynamic Programming/Climb Stairs With Minimum Moves.cpp
@@ -0,0 +1,31 @@
+/*
+1. You are given a number n, representing the number of stairs in a staircase.
+2. You are on the 0th step and are required to climb to the top.
+3. You are given n numbers, where ith element's value represents - till how far from the step you 
+     could jump to in a single move.  You can of-course fewer number of steps in the move.
+4. You are required to print the number of minimum moves in which you can reach the top of 
+     staircase.
+Note -> If there is no path through the staircase print null.
+Input Format
+A number n
+.. n more elements
+Output Format
+A number representing the number of ways to climb the stairs from 0 to top.
+Constraints
+0 <= n <= 20
+0 <= n1, n2, .. <= 20
+Sample Input
+10
+3
+3
+0
+2
+1
+2
+4
+2
+0
+0
+Sample Output
+4
+*/
