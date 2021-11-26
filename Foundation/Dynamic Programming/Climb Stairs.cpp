07be3ab69dc4449025494d6f285e2a8d3a300788
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-26 20:47:43 +0530

    Update Climb Stairs.cpp

--- Foundation/Dynamic Programming/Climb Stairs.cpp
+++ Foundation/Dynamic Programming/Climb Stairs.cpp
@@ -15,5 +15,49 @@ Sample Output
 7
 */
 //Using Memoization
+#include <iostream>
+using namespace std;
 
+int cs(int n, int qb[]){
+    // write your code here
+if(n==0) return 1;
+else if(n<0) return 0;
+if(qb[n]>0) return qb[n];
+int nm1=cs(n-1,qb);
+int nm2=cs(n-2,qb);
+int nm3=cs(n-3,qb);
+int cp=nm1+nm2+nm3;
+qb[n]=cp;
+    return cp;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<cs(n,new int[n+1])<<endl;
+}
 //Using Tabulation (Iteration)
+#include <iostream>
+using namespace std;
+
+int cs(int n){
+    // write your code here
+    int dp[n+1];
+    dp[0]=1;
+for(int i=1;i<=n;i++){
+    if(i==1)
+    dp[i]=dp[i-1];
+    else if(i==2) 
+    dp[i]=dp[i-1]+dp[i-2];
+    else
+    dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+}
+return dp[n];
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<cs(n)<<endl;
+}
