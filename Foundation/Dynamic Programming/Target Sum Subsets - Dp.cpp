18b08f37cd6f8876986969c5806623869751d4e5
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-27 00:37:56 +0530

    Update Target Sum Subsets - Dp.cpp

--- Foundation/Dynamic Programming/Target Sum Subsets - Dp.cpp
+++ Foundation/Dynamic Programming/Target Sum Subsets - Dp.cpp
@@ -28,3 +28,46 @@ Sample Output
 true
 */
 //No. of Subsets: 2^n (Each Element has 2 choices, whether to come or not)
+//Time Complexity: O(sum*n), where sum is the ‘target sum’ and ‘n’ is the size of array
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void targetSumSubset(int n, vector<int> arr, int tar) {
+
+// write your code here
+bool dp[n+1][tar+1];
+for(int i=0;i<n+1;i++){
+    for(int j=0;j<tar+1;j++){
+        if(i==0 && j==0)
+        dp[i][j]=true;
+        else if(i==0) dp[i][j]=false;
+        else if(j==0) dp[i][j]= true;
+        else if(dp[i-1][j]==true) dp[i][j]=true;
+        else {
+            int val=arr[i-1];
+            if(j>=val){
+                if(dp[i-1][j-val]==true)  dp[i][j]=true;
+            }
+        }
+        
+    }
+}
+ if (dp[n][tar] == 1) {
+    cout << "true";
+  } else cout << "false";
+}
+
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (int i = 0; i < arr.size(); i++) {
+    cin >> arr[i];
+  }
+  int tar;
+  cin >> tar;
+  targetSumSubset(n, arr, tar);
+
+}
