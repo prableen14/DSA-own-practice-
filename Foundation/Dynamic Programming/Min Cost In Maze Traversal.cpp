ca0a4dff29a60ba044e694642b6ceba04561f3d5
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-28 00:59:53 +0530

    Update Min Cost In Maze Traversal.cpp

--- Foundation/Dynamic Programming/Min Cost In Maze Traversal.cpp
+++ Foundation/Dynamic Programming/Min Cost In Maze Traversal.cpp
@@ -33,3 +33,44 @@ Sample Input
 Sample Output
 23
 */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int minCost(int n, int m, vector<vector<int>>& arr, vector<vector<int>>& dp ) {
+
+  // write your code here
+ 
+for(int i=n-1;i>=0;i--){
+     for(int j=m-1;j>=0;j--){
+         if(i==n-1 && j==m-1) dp[i][j]=arr[i][j];
+         else if(i==n-1) dp[i][j]=dp[i][j+1]+arr[i][j];
+         else if(j==m-1) dp[i][j]=dp[i+1][j]+arr[i][j];
+         else dp[i][j]= min(dp[i+1][j], dp[i][j+1])+arr[i][j];
+     }
+ }
+return dp[0][0];
+}
+
+int main()  {
+
+  int n;
+  int m;
+
+  cin >> n >> m;
+
+  vector<vector<int>> arr(n, vector<int>(m));
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> arr[i][j];
+    }
+  }
+
+  vector<vector<int>> dp(n, vector<int>(m));
+
+  cout << minCost(n, m, arr, dp);
+}
