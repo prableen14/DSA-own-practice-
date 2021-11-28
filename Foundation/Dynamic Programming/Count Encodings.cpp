64dc35834fa621e08d0d36c36410a7d248bdeed7
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-29 02:56:47 +0530

    Update Count Encodings.cpp

--- Foundation/Dynamic Programming/Count Encodings.cpp
+++ Foundation/Dynamic Programming/Count Encodings.cpp
@@ -24,3 +24,32 @@ Constraints
 Sample Input
 123
 */
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    int dp[str.size()];
+    dp[0]=1;
+    for(int i=1;i<str.size();i++){
+        if(str.at(i-1)=='0' && str.at(i)=='0'){
+          dp[i]=0;  
+        }
+        else if(str.at(i-1)=='0' && str.at(i)!='0'){
+            dp[i]=dp[i-1];
+        }
+        else if(str.at(i-1)!='0' && str.at(i)=='0'){
+            if(str.at(i-1)=='1' || str.at(i-1)=='2'){
+                dp[i]=(i>=2? dp[i-2]: 1);
+            }
+            else dp[i]=0;
+        }
+        else{
+            if(stoi(str.substr(i-1,i+1))<=26){
+                dp[i]=dp[i-1]+(i>=2? dp[i-2]: 1);
+            }
+            else dp[i]=dp[i-1];
+        }
+    }
+    cout<<dp[str.size()-1];
+}
