684491919a980ad281fe16aebceb2ad3582f6e2c
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-28 10:15:58 +0530

    Create Minimum Number Of Platform.cpp

---
+++ Level Up/Minimum Number Of Platform.cpp
@@ -0,0 +1,22 @@
+/*
+1. We have arrival and departure times of all trains that reach a railway station. 
+2. We have to find the minimum number of platforms required for the railway station so that no train is kept waiting.
+3. Consider that all the trains arrive on the same day and leave on the same day. 
+4. Arrival and departure time can never be the same for a train but we can have arrival time of one train equal to departure time of the other. 
+5. At any given instance of time, same platform can not be used for both departure of a train and arrival of another train. In such cases, we need different platforms.
+Input Format
+n = 6 
+arr[] = {0900, 0940, 0950, 1100, 1500, 1800}
+dep[] = {0910, 1200, 1120, 1130, 1900, 2000}
+Output Format
+3
+Constraints
+1. 1 <= no. of trains <= 10000
+2. 0000 <= Arrival[i] < Departure[i] <= 2359
+Sample Input
+6 
+0900 0940 0950 1100 1500 1800
+0910 1200 1120 1130 1900 2000
+Sample Output
+3
+*/
