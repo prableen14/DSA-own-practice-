b84bc4b5f8ee5881282b396be501c4e00d42e1b1
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-29 03:03:47 +0530

    Update Count Encodings.cpp

--- Foundation/Dynamic Programming/Count Encodings.cpp
+++ Foundation/Dynamic Programming/Count Encodings.cpp
@@ -24,6 +24,53 @@ Constraints
 Sample Input
 123
 */
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int encode(char *str,int n){
+    int dp[n];
+    memset(dp,0,sizeof(dp));
+    
+    dp[0]=1;
+    
+    for(int i=1;i<=n;i++){
+        if(str[i-1]=='0' && str[i]=='0'){
+            dp[i]=0;
+        } 
+        else if(str[i-1]=='0' && str[i]!='0'){
+            dp[i]=dp[i-1];
+        }else if(str[i-1]!='0' && str[i]=='0'){
+            if(str[i-1]=='2' || str[i-1]=='1'){
+                dp[i]= (i>=2 ? dp[i-2] : 1);
+            }else{
+                dp[i]=0;
+            }
+        }
+        else{
+            if (str[i - 1] == '1'
+        || (str[i - 1] == '2'
+        && str[i] < '7')){
+                dp[i]=dp[i-1] + (i>=2 ? dp[i-2] : 1);
+            }else{
+                dp[i]=dp[i-1];
+            }
+        }
+     }
+     
+     return dp[n-1];
+}
+
+int main(){
+    
+    char str[11];
+    cin>>str;
+    
+    int n=strlen(str);
+    
+    cout<<encode(str,n)<<endl;
+}
+//This following is one not giving giving right output
 #include <iostream>
 #include<string>
 using namespace std;
