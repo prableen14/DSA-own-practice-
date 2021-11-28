149ee708f87f25c73366c0bacaa0005aedc4a8cb
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-29 02:15:25 +0530

    Create Count Encodings.cpp

---
+++ Foundation/Dynamic Programming/Count Encodings.cpp
@@ -0,0 +1,26 @@
+/*
+1. You are given a string str of digits. (will never start with a 0)
+2. You are required to encode the str as per following rules
+    1 -> a
+    2 -> b
+    3 -> c
+    ..
+    25 -> y
+    26 -> z
+3. You are required to calculate and print the count of encodings for the string str.
+
+     For 123 -> there are 3 encodings. abc, aw, lc
+     For 993 -> there is 1 encoding. iic 
+     For 013 -> This is an invalid input. A string starting with 0 will not be passed.
+     For 103 -> there is 1 encoding. jc
+     For 303 -> there are 0 encodings. But such a string maybe passed. In this case 
+     print 0.
+Input Format
+A string str
+Output Format
+count of encodings
+Constraints
+0 < str.length <= 10
+Sample Input
+123
+*/
