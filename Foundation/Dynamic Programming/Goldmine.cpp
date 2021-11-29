0483b4e795d24dd9d107b2a7e0e9af7476cee5b1
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-11-29 05:47:55 +0530

    Update Goldmine.cpp

--- Foundation/Dynamic Programming/Goldmine.cpp
+++ Foundation/Dynamic Programming/Goldmine.cpp
@@ -33,3 +33,41 @@ Sample Input
 Sample Output
 33
 */
+#include <bits/stdc++.h>
+using namespace std;
+int collectGold(int n, int m, vector<vector<int>>& arr, vector<vector<int>>& dp) {
+
+  //write your code here
+for(int j=m-1;j>=0;j--){
+     for(int i=n-1;i>=0;i--){
+         if(j==m-1) dp[i][j]=arr[i][j];
+         else if(i==0){
+             dp[i][j]=arr[i][j]+max(dp[i][j+1],dp[i+1][j+1]);
+         }
+        else if(i==n-1){
+            dp[i][j]=arr[i][j]+max(dp[i][j+1], dp[i-1][j+1]);
+        }
+        else {
+            dp[i][j]=arr[i][j]+ max ( max(dp[i - 1][j + 1], dp[i + 1][j + 1]), dp[i][j + 1] );
+        }
+     }  
+}
+int maxGold=dp[0][0];
+for(int i=0;i<dp.size();i++){
+     maxGold = max(maxGold, dp[i][0]);
+}
+  return maxGold;
+}
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<int>> arr(n, vector<int>(m, 0));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> arr[i][j];
+    }
+  }
+  vector<vector<int>> dp(n, vector<int>(m, 0));
+  cout << collectGold(n, m, arr, dp);
+}
