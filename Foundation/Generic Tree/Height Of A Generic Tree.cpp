a97fc76f6449ed01df2f177e60e7946292fe1e67
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-03 05:19:21 +0530

    Create Height Of A Generic Tree.cpp

---
+++ Foundation/Generic Tree/Height Of A Generic Tree.cpp
@@ -0,0 +1,16 @@
+/*
+1. You are given a partially written GenericTree class.
+2. You are required to complete the body of height function. The function is expected to find the height of tree. Depth of a node is defined as the number of edges it is away from the root (depth of root is 0). Height of a tree is defined as depth of deepest node.
+3. Input and Output is managed for you.
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+Constraints
+None
+Sample Input
+12
+10 20 -1 30 50 -1 60 -1 -1 40 -1 -1
+Sample Output
+2
+*/
