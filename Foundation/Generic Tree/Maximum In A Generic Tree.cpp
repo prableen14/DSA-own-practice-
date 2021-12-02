fcedcdd4febfb64b73addde451ceed0e56f87a3f
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-03 02:28:15 +0530

    Create Maximum In A Generic Tree.cpp

---
+++ Foundation/Generic Tree/Maximum In A Generic Tree.cpp
@@ -0,0 +1,16 @@
+/*
+1. You are given a partially written GenericTree class.
+2. You are required to complete the body of max function. The function is expected to find the node with maximum value and return it.
+3. Input and Output is managed for you.
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+Constraints
+None
+Sample Input
+12
+10 20 -1 30 50 -1 60 -1 -1 40 -1 -1
+Sample Output
+60
+*/
