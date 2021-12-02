ff2a50d205a07ee569b70a53e6f8bba976d14a44
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-02 22:15:14 +0530

    Create Buy And Sell Stocks - Two Transactions Allowed.cpp

---
+++ Foundation/Dynamic Programming/Buy And Sell Stocks - Two Transactions Allowed.cpp
@@ -0,0 +1,27 @@
+/*
+1. You are given a number n, representing the number of days.
+2. You are given n numbers, where ith number represents price of stock on ith day.
+3. You are required to print the maximum profit you can make if you are allowed two transactions at-most.
+Note - There can be no overlapping transaction. One transaction needs to be closed (a buy followed by a sell) before opening another transaction (another buy).
+Input Format
+A number n
+.. n more elements
+Output Format
+A number representing the maximum profit you can make if you are allowed a single transaction.
+Constraints
+0 <= n <= 20
+0 <= n1, n2, .. <= 10
+Sample Input
+9
+11
+6
+7
+19
+4
+1
+6
+18
+4
+Sample Output
+30
+*/
