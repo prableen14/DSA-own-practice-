7831d0b215e3d136ff5243d24d826d27339f32f8
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-03 03:19:02 +0530

    Update Buy And Sell Stocks - K Transactions Allowed.cpp

--- Foundation/Dynamic Programming/Buy And Sell Stocks - K Transactions Allowed.cpp
+++ Foundation/Dynamic Programming/Buy And Sell Stocks - K Transactions Allowed.cpp
@@ -71,3 +71,22 @@ int main() {
   transactions(arr, k);
   return 0;
 }
+//OPTIMIZED SOLUTION, T(N): O(N^2)
+
+void transactions(vector<int> arr, int k) {
+  //write your code here
+  int n=arr.size();
+   vector<vector<int>> dp(k + 1, vector<int>(n, 0));
+  for(int t=1;t<=k;t++){
+       int max=INT_MIN;
+      for(int d=1;d<arr.size();d++){
+          if(dp[t-1][d-1]-arr[d-1]>max)
+          max=dp[t-1][d-1]-arr[d-1];
+          if(max+arr[d]>dp[t][d-1])
+          dp[t][d]=max+arr[d];
+          else dp[t][d]=dp[t][d-1];
+
+      }
+  }
+  cout<<dp[k][n-1];
+}
