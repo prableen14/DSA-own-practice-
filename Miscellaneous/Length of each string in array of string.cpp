8a37c7737db4b3bd52fa911c19729612da741227
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-04 23:56:51 +0530

    Update Length of each string in array of string.cpp

--- Miscellaneous/Length of each string in array of string.cpp
+++ Miscellaneous/Length of each string in array of string.cpp
@@ -21,3 +21,28 @@ cout<<arr[i];
 
     return 0;
 }
+//Another
+#include <iostream>
+#include <bits/stdc++.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<string> arr(n);
+    vector<string> cha(n);
+    for(int i=0;i<n;i++)
+    cin>>cha[i];
+ 
+
+for(int i=0;i<cha.size();i++){
+    arr[i]=cha.at(i);
+    int len=0;
+    for(int j=0;j<arr.at(i).size();j++){
+        len++;
+    }
+    cout<<len<<endl;
+}
+    return 0;
+}
