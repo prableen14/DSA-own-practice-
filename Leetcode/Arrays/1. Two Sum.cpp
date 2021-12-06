45a8d9886dc41076b2a796b2c7e2f4e280aea6d2
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-07 03:10:35 +0530

    Update and rename TwoSum.cpp to 1. Two Sum.cpp

---
+++ Leetcode/Arrays/1. Two Sum.cpp
@@ -0,0 +1,63 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+*/
+//Brute force solution
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int a=0;
+        int b=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    a=i;
+                    b=j;
+                }
+            }
+        }
+        return {a,b};
+    }
+};
+//Optimised
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])==mp.end())
+                mp[nums[i]]=i;
+            if(mp.find(target-nums[i])!=mp.end() && i!=mp[target-nums[i]]){
+                res.push_back(i);
+                res.push_back(mp[target-nums[i]]);
+                break;
+            }
+        }
+        return res;
+    }
+};
