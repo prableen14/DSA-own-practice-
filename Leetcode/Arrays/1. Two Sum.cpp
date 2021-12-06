1e6c8edf1ceba58a00f2f59f8d78d33263f13c0b
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-07 03:17:40 +0530

    Update 1. Two Sum.cpp

--- Leetcode/Arrays/1. Two Sum.cpp
+++ Leetcode/Arrays/1. Two Sum.cpp
@@ -51,7 +51,7 @@ public:
         vector<int> res;
         for(int i=0;i<nums.size();i++){
             if(mp.find(nums[i])==mp.end())
-                mp[nums[i]]=i;
+                mp[nums[i]]=i; //2->0, 7->1 ..
             if(mp.find(target-nums[i])!=mp.end() && i!=mp[target-nums[i]]){
                 res.push_back(i);
                 res.push_back(mp[target-nums[i]]);
