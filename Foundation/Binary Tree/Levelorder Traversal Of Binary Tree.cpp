e5901013acab17a466de28925452503ecec42c94
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-11 06:53:10 +0530

    Update Levelorder Traversal Of Binary Tree.cpp

--- Foundation/Binary Tree/Levelorder Traversal Of Binary Tree.cpp
+++ Foundation/Binary Tree/Levelorder Traversal Of Binary Tree.cpp
@@ -17,3 +17,94 @@ Sample Output
 12 37 62 87 
 30 70 
 */
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+
+using namespace std;
+class Node {
+public:
+  int data;
+  Node* left = nullptr;
+  Node* right = nullptr;
+  Node(int data) {
+    this->data = data;
+  }
+};
+
+class Pair {
+public:
+  Node* node = nullptr;
+  int state = 0;
+  Pair(Node* node, int state) {
+    this->node = node;
+    this->state = state;
+  }
+};
+
+int idx = 0;
+Node* constructTree(vector<int>& arr) {
+  if (idx == arr.size() || arr[idx] == -1) {
+    idx++;
+    return nullptr;
+  }
+  Node* node = new Node(arr[idx++]);
+  node->left = constructTree(arr);
+  node->right = constructTree(arr);
+  return node;
+}
+
+void display(Node* node) {
+  if (node == nullptr)
+    return;
+  string str = "";
+  str += node->left != nullptr ? to_string(node->left->data) : ".";
+  str += " <- " + to_string(node->data) + " -> ";
+  str += node->right != nullptr ? to_string(node->right->data) : ".";
+  cout << str << endl;
+  display(node->left);
+  display(node->right);
+}
+
+
+void levelOrder(Node* node) {
+  // write your code here
+   queue<Node*> mq;
+  mq.push(node);
+
+  while (mq.size() > 0) {
+    int count = mq.size();
+
+    for (int i = 0; i < count; i++) {
+      node = mq.front();
+      mq.pop();
+      cout << node->data << " ";
+
+      if (node->left != nullptr) {
+        mq.push(node->left);
+      }
+
+      if (node->right != nullptr) {
+        mq.push(node->right);
+      }
+    }
+    cout << endl;
+}
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<int> arr(n, 0);
+  for (int i = 0; i < n; i++) {
+    string temp;
+    cin >> temp;
+    if (temp == "n") {
+      arr[i] = -1;
+    } else {
+      arr[i] = stoi(temp);
+    }
+  }
+  Node* root = constructTree(arr);
+  levelOrder(root);
+}
