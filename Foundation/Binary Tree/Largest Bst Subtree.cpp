9a6d5f144bc70d6c7c4fa3a4e7ed5722c1d4f248
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-22 20:02:10 +0530

    Create Largest Bst Subtree.cpp

---
+++ Foundation/Binary Tree/Largest Bst Subtree.cpp
@@ -0,0 +1,19 @@
+/*
+1. You are given a partially written BinaryTree class.
+2. You are required to find the root of largest sub-tree which is a BST. Also, find the number of nodes in that sub-tree.
+3. Input is managed for you. 
+
+Note -> Please refer the question video for clarity.
+Input Format
+Input is managed for you.
+Output Format
+@
+Constraints
+Time complexity must be O(n)
+Space should not be more than required for recursion (call-stack)
+Sample Input
+15
+50 25 12 n n 37 n n 75 62 n n 87 n n
+Sample Output
+50@7
+*/
