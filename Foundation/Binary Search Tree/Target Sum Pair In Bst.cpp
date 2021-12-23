b7d08bd2ddb229b9fddfda088533f82914291e09
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-23 14:15:25 +0530

    Create Target Sum Pair In Bst.cpp

---
+++ Foundation/Binary Search Tree/Target Sum Pair In Bst.cpp
@@ -0,0 +1,20 @@
+/*
+1. You are given a partially written BST class.
+2. You are given a value. You are required to print all pair of nodes which add up to the given value. 
+Make sure all pairs print the smaller value first and avoid duplicacies. Make sure to print the pairs in increasing order.
+3. Input and Output is managed for you. 
+Input Format
+Input is managed for you
+Output Format
+"smaller node" "larger node"
+.. all pairs that add to target on separate lines
+Constraints
+None
+Sample Input
+21
+50 25 12 n n 37 30 n n n 75 62 60 n n 70 n n 87 n n
+100
+Sample Output
+25 75
+30 70
+*/
