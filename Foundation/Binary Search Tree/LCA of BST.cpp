de11e457ed5e991b0edae9ff00928754da3fdb10
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-23 14:17:34 +0530

    Update and rename Foundation/Binary Tree/Binary Search Tree/LCA of BST.cpp to Foundation/Binary Search Tree/LCA of BST.cpp

---
+++ Foundation/Binary Search Tree/LCA of BST.cpp
@@ -0,0 +1,18 @@
+/*1. You are given a partially written BST class.
+2. You are required to complete the body of lca function. "lca" function is expected to find the lowest commong ancestor of d1 and d2.
+3. Input and Output is managed for you. 
+
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+Constraints
+None
+Sample Input
+21
+50 25 12 n n 37 30 n n n 75 62 60 n n 70 n n 87 n n
+12
+30
+Sample Output
+25
+*/
