57bfe504add490e2d5fc872a81501cda33d78964
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-24 23:04:52 +0530

    Create Replace With Sum Of Larger.cpp

---
+++ Foundation/Binary Search Tree/Replace With Sum Of Larger.cpp
@@ -0,0 +1,24 @@
+/*
+1. You are given a partially written BST class.
+2. You are required to complete the body of rwsol function. "rwsol" function is expected to replace a node's value with sum of all nodes greater than it.
+3. Input and Output is managed for you. 
+
+Note ->  Use the statis sum data member to complete your code.
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+Constraints
+None
+Sample Input
+15
+50 25 12 n n 37 n n 75 62 n n 87 n n
+Sample Output
+311 <- 224 -> 87
+336 <- 311 -> 274
+. <- 336 -> .
+. <- 274 -> .
+162 <- 87 -> 0
+. <- 162 -> .
+. <- 0 -> .
+*/
