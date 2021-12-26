0671a2ff62570725e537c523ef30d595c47f945a
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-26 13:25:07 +0530

    Create Remove Node from BST.cpp

---
+++ Foundation/Binary Search Tree/Remove Node from BST.cpp
@@ -0,0 +1,22 @@
+/*1. You are given a partially written BST class.
+2. You are required to complete the body of remove function. "remove" function is expected to remove a new node with given data to the tree and return the new root.
+3. Input and Output is managed for you. 
+
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+Constraints
+None
+Sample Input
+15
+50 25 12 n n 37 n n 75 62 n n 87 n n
+62
+Sample Output
+25 <- 50 -> 75
+12 <- 25 -> 37
+. <- 12 -> .
+. <- 37 -> .
+. <- 75 -> 87
+. <- 87 -> .
+*/
