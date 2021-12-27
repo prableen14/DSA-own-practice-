f45614a29e7e1779baf262ef55433cd86682de39
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-27 16:48:27 +0530

    Update 476. Number Complement.cpp

--- Leetcode/Bit Manipulation/476. Number Complement.cpp
+++ Leetcode/Bit Manipulation/476. Number Complement.cpp
@@ -33,3 +33,30 @@ public:
         return ans;
     }
 };
+
+//Using array, naive approach
+class Solution {
+public:
+    int findComplement(int n) {
+        vector<int> arr;
+        // creating binary array representation for the number
+        while(n) {
+            arr.push_back(n % 2);
+            n /= 2;
+        }
+        int sz = arr.size();
+        // Since, we are retrieving bits in backward fashion so we need to reverse it.
+        reverse(arr.begin(), arr.end());
+        // switching bit values
+        for(int i = 0; i<sz; ++i) {
+              arr[i] = !arr[i];
+        }
+        long long t = 1;
+        // converting our above array into decimal form.
+        for(int i = sz-1; i>=0; --i) {
+            n += arr[i] * t;
+            t *= 2;
+        }
+        return n;
+    }
+};
