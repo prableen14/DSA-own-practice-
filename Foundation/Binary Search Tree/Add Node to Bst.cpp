4f6c6154bcdbf14b028269defb53d60541a8e924
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-29 22:34:31 +0530

    Update Add Node to Bst.cpp

--- Foundation/Binary Search Tree/Add Node to Bst.cpp
+++ Foundation/Binary Search Tree/Add Node to Bst.cpp
@@ -1,1 +1,27 @@
-
+/*1. You are given a partially written BST class.
+2. You are required to complete the body of add function. "add" function is expected to add a new node with given data to the tree and return the new root.
+3. Input and Output is managed for you.
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+Constraints
+None
+Sample Input
+23
+50 25 12 n n 37 30 n n 40 n n 75 62 60 n n 70 n n 87 n n
+61
+Sample Output
+25 <- 50 -> 75
+12 <- 25 -> 37
+. <- 12 -> .
+30 <- 37 -> 40
+. <- 30 -> .
+. <- 40 -> .
+62 <- 75 -> 87
+60 <- 62 -> 70
+. <- 60 -> 61
+. <- 61 -> .
+. <- 70 -> .
+. <- 87 -> .
+*/
