b03b0fcc8c9d75ddeb2e6cbe568706eb75d093e1
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-31 18:43:58 +0530

    Create Highest Frequency Character.cpp

---
+++ Foundation/Hashmap and Heap/Highest Frequency Character.cpp
@@ -0,0 +1,15 @@
+/*
+1. You are given a string str.
+2. You are required to find the character with maximum frequency.
+Input Format
+A string str
+Output Format
+The character with highest frequency
+Constraints
+0 < str.length() <= 100
+There will be a single character with highest frequency
+Sample Input
+zmszeqxllzvheqwrofgcuntypejcxovtaqbnqyqlmrwitc
+Sample Output
+q
+*/
