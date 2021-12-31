688ce5c77c6e941935906f9f639d49111bda382b
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2021-12-31 19:09:44 +0530

    Create Has Path?.cpp

---
+++ Foundation/Graphs/Has Path?.cpp
@@ -0,0 +1,26 @@
+/*
+1. You are given a graph, a src vertex and a destination vertex.
+2. You are required to find if a path exists between src and dest. If it does, print true 
+     otherwise print false.
+Input Format
+Input has been managed for you
+Output Format
+true if path exists, false otherwise
+Constraints
+None
+Sample Input
+7
+8
+0 1 10
+1 2 10
+2 3 10
+0 3 10
+3 4 10
+4 5 10
+5 6 10
+4 6 10
+0
+6
+Sample Output
+true
+*/
