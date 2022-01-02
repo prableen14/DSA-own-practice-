3f8ce6dd8d56e01e8588c7a4121c24169c09e4f7
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-02 20:59:38 +0530

    Create Print All Paths.cpp

---
+++ Foundation/Graphs/Print All Paths.cpp
@@ -0,0 +1,35 @@
+/*
+1. You are given a graph, a source vertex and a destination vertex.
+2. You are required to find and print all paths between source and destination. Print them in lexicographical order.
+    E.g. Check the following paths
+             012546
+             01256
+             032546
+             03256
+
+    The lexicographically smaller path is printed first.
+Input Format
+Input has been managed for you
+Output Format
+Check sample output
+Constraints
+None
+Sample Input
+7
+8
+0 1 10
+1 2 10
+2 3 10
+0 3 10
+3 4 10
+4 5 10
+5 6 10
+4 6 10
+0
+6
+Sample Output
+0123456
+012346
+03456
+0346
+*/
