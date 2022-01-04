53eab63dbdc13496794828cc072b8f56cf91215e
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-04 11:04:01 +0530

    Create Get Connected Components Of A Graph.cpp

---
+++ Foundation/Graphs/Get Connected Components Of A Graph.cpp
@@ -0,0 +1,20 @@
+/*
+1. You are given a graph.
+2. You are required to find and print all connected components of the graph.
+Input Format
+Input has been managed for you
+Output Format
+Check the sample output
+Constraints
+None
+Sample Input
+7
+5
+0 1 10
+2 3 10
+4 5 10
+5 6 10
+4 6 10
+Sample Output
+[[0, 1], [2, 3], [4, 5, 6]]
+*/
