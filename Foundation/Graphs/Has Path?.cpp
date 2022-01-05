bbdc764f82f0fb743a727533bf1b49495c014068
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-05 21:13:04 +0530

    Update Has Path?.cpp

--- Foundation/Graphs/Has Path?.cpp
+++ Foundation/Graphs/Has Path?.cpp
@@ -24,3 +24,66 @@ Sample Input
 Sample Output
 true
 */
+#include<bits/stdc++.h>
+using namespace std;
+class Edge {
+public:
+  int src;
+  int nbr;
+  int wt;
+
+  Edge(int src, int nbr, int wt) {
+    this->src = src;
+    this->nbr = nbr;
+    this->wt = wt;
+  }
+};
+bool hasPath(vector<Edge>graph[],int src, int dest,vector<bool> visited){
+    if(src==dest) return true;
+    visited[src]=true;
+    for(Edge edge: graph[src]){
+        if(visited[edge.nbr]==false){
+            bool hasNbrPath=hasPath(graph, edge.nbr,dest,visited);
+            if(hasNbrPath==true) return true;
+        }
+    }
+    return false;
+}
+int main() {
+  int vtces;
+  cin >> vtces;
+  vector<Edge>graph[vtces];
+
+  int edges;
+  cin >> edges;
+  for (int i = 0; i < edges; i++) {
+    int v1 ;
+    int v2 ;
+    int wt ;
+    cin >> v1 >> v2 >> wt;
+    graph[v1].push_back( Edge(v1, v2, wt));
+    graph[v2].push_back( Edge(v2, v1, wt));
+  }
+  int src;
+  cin >> src;
+  int dest;
+  cin >> dest;
+  // write your code here
+vector<bool> visited(vtces, false);
+ if (hasPath(graph, src, dest, visited)) {
+    cout << "true" << endl;
+  } else {
+    cout << "false" << endl;
+  }
+}
+
+//Another way to write the main checking statement
+bool hasPath(vector<Edge>graph[], int  src, int dest, vector<bool> vis) {
+  if (src == dest)return true;
+  vis[src] = true;
+  for (Edge ed : graph[src]) {
+    if (vis[ed.nbr] == false && hasPath(graph, ed.nbr, dest, vis))
+      return true;
+  }
+  return false;
+}
