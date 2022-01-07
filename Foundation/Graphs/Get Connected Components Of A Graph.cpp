cb1633652074e81a760df757151a3674af97fff3
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-07 14:59:20 +0530

    Update Get Connected Components Of A Graph.cpp

--- Foundation/Graphs/Get Connected Components Of A Graph.cpp
+++ Foundation/Graphs/Get Connected Components Of A Graph.cpp
@@ -18,3 +18,69 @@ Sample Input
 Sample Output
 [[0, 1], [2, 3], [4, 5, 6]]
 */
+#include<bits/stdc++.h>
+using namespace std;
+class Edge {
+public:
+  int src;
+  int nbr;
+  int wt;
+
+  Edge(int src, int nbr, int wt) {
+    this->src = src;
+    this->nbr = nbr;
+    this->wt = wt;
+  }
+};
+void getCmp(vector<Edge>graph[], vector<bool>& vis, int src, vector<int>& comp) {
+  vis[src] = true;
+  comp.push_back(src);
+  for (Edge ed : graph[src]) {
+    if (!vis[ed.nbr]) {
+      getCmp(graph, vis, ed.nbr, comp);
+    }
+  }
+}
+int main() {
+  int vtces;
+  cin >> vtces;
+  vector<Edge>graph[vtces];
+
+  int edges;
+  cin >> edges;
+  for (int i = 0; i < edges; i++) {
+    int v1 ;
+    int v2 ;
+    int wt ;
+    cin >> v1 >> v2 >> wt;
+    graph[v1].push_back( Edge(v1, v2, wt));
+    graph[v2].push_back( Edge(v2, v1, wt));
+  }
+  vector<vector<int>> comps;
+  vector<bool> vis(vtces, false);
+
+  for (int i = 0 ; i < vtces ; i++) {
+    if (vis[i] == false) {
+      vector<int>comp;
+      getCmp(graph, vis, i, comp);
+      comps.push_back(comp);
+    }
+  }
+  int count = 0;
+  for (int i = 0; i < comps.size(); i++) {
+    count += comps[i].size();
+  }
+  cout << "[";
+  for (int i = 0 ; i < comps.size() ; i++) {
+    cout << "[";
+    for (int j = 0 ; j < comps[i].size() ; j++) {
+      if (j != comps[i].size() - 1)
+        cout << comps[i][j] << ", ";
+      else
+        cout << comps[i][j];
+    }
+    cout << "]";
+    if (i != comps.size() - 1)cout << ", ";
+  }
+  cout << "]";
+}
