f5612e9e301b718696950848a38db5ac6112837c
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-11 00:21:06 +0530

    Create Spread Of Infection.cpp

---
+++ Foundation/Graphs/Spread Of Infection.cpp
@@ -0,0 +1,26 @@
+/*
+1. You are given a graph, representing people and their connectivity.
+2. You are also given a src person (who got infected) and time t.
+3. You are required to find how many people will get infected in time t, if the infection spreads to neighbors of infected person in 1 unit of time.
+Input Format
+Input has been managed for you
+Output Format
+count of people infected by time t
+Constraints
+None
+Sample Input
+7
+8
+0 1 10
+1 2 10
+2 3 10
+0 3 10
+3 4 10
+4 5 10
+5 6 10
+4 6 10
+6
+3
+Sample Output
+4
+*/
