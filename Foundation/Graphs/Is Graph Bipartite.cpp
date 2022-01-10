f7ca7b810196ed185ae735342606000c19e362ba
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-10 22:56:16 +0530

    Create Is Graph Bipartite.cpp

---
+++ Foundation/Graphs/Is Graph Bipartite.cpp
@@ -0,0 +1,26 @@
+/*
+1. You are given a graph.
+2. You are required to find and print if the graph is bipartite
+
+Note -> A graph is called bipartite if it is possible to split it's vertices in two sets of mutually 
+               exclusive and exhaustive vertices such that all edges are across sets.
+Input Format
+Input has been managed for you
+Output Format
+true if the graph is bipartite, false otherwise
+Constraints
+None
+Sample Input
+7
+8
+0 1 10
+1 2 10
+2 3 10
+0 3 10
+3 4 10
+4 5 10
+5 6 10
+4 6 10
+Sample Output
+false
+*/
