57e82ddcb2332ae071f7dd0d57052d993d149429
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-22 23:16:22 +0530

    Create Duplicate Brackets.cpp

---
+++ Foundation/Stacks and Queues/Duplicate Brackets.cpp
@@ -0,0 +1,20 @@
+/*
+1. You are given a string exp representing an expression.
+2. Assume that the expression is balanced  i.e. the opening and closing brackets match with each other.
+3. But, some of the pair of brackets maybe extra/needless. 
+4. You are required to print true if you detect extra brackets and false otherwise.
+
+e.g.'
+((a + b) + (c + d)) -> false
+(a + b) + ((c + d)) -> true
+Input Format
+A string str
+Output Format
+true or false
+Constraints
+0 <= str.length <= 100
+Sample Input
+(a + b) + ((c + d))
+Sample Output
+true
+*/
