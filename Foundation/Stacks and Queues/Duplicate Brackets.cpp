d3e71eab747a34504fb0f2de6181ca3b74246327
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-24 23:44:27 +0530

    Update Duplicate Brackets.cpp

--- Foundation/Stacks and Queues/Duplicate Brackets.cpp
+++ Foundation/Stacks and Queues/Duplicate Brackets.cpp
@@ -18,3 +18,28 @@ Sample Input
 Sample Output
 true
 */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    getline(cin,str);
+    stack<char>st;
+    string ans="false";
+    for(int i = 0; i < str.size(); i++){
+            char ch = str.at(i);
+            if(ch == ')'){
+                if(st.top() == '('){
+                    ans="true";
+                    
+                } else {
+                    while(st.top() != '('){
+                        st.pop();
+                    }
+                    st.pop();
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+cout<<ans;
+}
