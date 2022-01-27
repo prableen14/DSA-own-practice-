5f70105e17ff3ba506bd6abb9bb253e9570a57e0
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-27 20:15:20 +0530

    Create Stock Span.cpp

---
+++ Foundation/Stacks and Queues/Stock Span.cpp
@@ -0,0 +1,26 @@
+/*
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing the prices of a share on n days.
+3. You are required to find the stock span for n days.
+4. Stock span is defined as the number of days passed between the current day and the first day before today when price was higher than today.
+
+e.g.
+for the array [2 5 9 3 1 12 6 8 7]
+span for 2 is 1
+span for 5 is 2
+span for 9 is 3
+span for 3 is 1
+span for 1 is 1
+span for 12 is 6
+span for 6 is 1
+span for 8 is 2
+span for 7 is 1
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+Constraints
+0 <= n < 10^5
+-10^9 <= a[i] <= 10^9
+For INPUT and OUTPUT example see the following link: https://www.pepcoding.com/resources/online-java-foundation/stacks-and-queues/stock-span-official/ojquestion#!
+*/
