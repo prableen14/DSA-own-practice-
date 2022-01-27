76f8ed3e0a3fba1975bff773848f54aefea097cf
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-28 02:37:05 +0530

    Create Smallest Number Following Pattern.cpp

---
+++ Foundation/Stacks and Queues/Smallest Number Following Pattern.cpp
@@ -0,0 +1,25 @@
+/*
+1. You are given a pattern of upto 8 length containing characters 'i' and 'd'.
+2. 'd' stands for decreasing and 'i' stands for increasing
+3. You have to print the smallest number, using the digits 1 to 9 only without repetition, such that 
+the digit decreases following a d and increases follwing an i.
+
+e.g.
+d -> 21
+i -> 12
+ddd -> 4321
+iii -> 1234
+dddiddd -> 43218765
+iiddd -> 126543
+Input Format
+Input is managed for you
+Output Format
+Smallest sequence of digits (from 1 to 9) without duplicacy and following the pattern
+Constraints
+0 < str.length <= 8
+str contains only 'd' and 'i'
+Sample Input
+ddddiiii
+Sample Output
+543216789
+*/
