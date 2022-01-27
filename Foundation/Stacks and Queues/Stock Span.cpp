f47167be0898aa030943c062f24deca568cd0274
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-27 22:26:58 +0530

    Update Stock Span.cpp

--- Foundation/Stacks and Queues/Stock Span.cpp
+++ Foundation/Stacks and Queues/Stock Span.cpp
@@ -24,3 +24,43 @@ Constraints
 -10^9 <= a[i] <= 10^9
 For INPUT and OUTPUT example see the following link: https://www.pepcoding.com/resources/online-java-foundation/stacks-and-queues/stock-span-official/ojquestion#!
 */
+#include <bits/stdc++.h>
+using namespace std;
+void display(vector<int>a) {
+  for (int i = 0; i < a.size(); i++)
+  {
+    cout << a[i] << endl;
+  }
+}
+vector<int> solve(vector<int>arr)
+{
+  //write your code here
+  stack<int> st;
+  st.push(0);
+  vector<int> span(arr.size(), 0);
+  span[0] = 1;
+  for (int i = 1; i < arr.size(); i++)
+  {
+    while (st.size() > 0 && arr[st.top()] <= arr[i]){
+        st.pop();
+    }
+    if(st.size()==0) span[i]=i+1; //no greater element
+    else span[i]=i-st.top(); //current element index - left greatest element's index
+    st.push(i);
+  }
+  return span;
+}
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int>arr(n, 0);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  vector<int>span(n, 0);
+  span = solve(arr);
+  display(span);
+  return 0;
+}
