5f8d1c9070d8ae918fd2accfd3420ef8183f0c16
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-28 02:30:44 +0530

    Create Largest Area Histogram.cpp

---
+++ Foundation/Stacks and Queues/Largest Area Histogram.cpp
@@ -0,0 +1,26 @@
+/*
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing the height of bars in a bar chart.
+3. You are required to find and print the area of largest rectangle in the histogram.
+
+e.g.
+for the array [6 2 5 4 5 1 6] -> 12
+Input Format
+Input is managed for you
+Output Format
+A number representing area of largest rectangle in histogram
+Constraints
+0 <= n < 20
+0 <= a[i] <= 10
+Sample Input
+7
+6
+2
+5
+4
+5
+1
+6
+Sample Output
+12
+*/
