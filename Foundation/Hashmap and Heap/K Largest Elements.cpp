aa83c843b6dddd0330166a1df10eb0d7a6154768
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-01-28 22:28:47 +0530

    Create K Largest Elements.cpp

---
+++ Foundation/Hashmap and Heap/K Largest Elements.cpp
@@ -0,0 +1,35 @@
+/*
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing elements of array a.
+3. You are given a number k.
+4. You are required to find and print the k largest elements of array in increasing order.
+Input Format
+Input is managed for you
+Output Format
+Print the k largest elements of array in increasing order in separate lines.
+Constraints
+1 <= n <= 30
+0 <= n1, n2, .. n elements <= 100
+0 < k <= n
+Sample Input
+13
+12
+62
+22
+15
+37
+99
+11
+37
+98
+67
+31
+84
+99
+4
+Sample Output
+84
+98
+99
+99
+*/
