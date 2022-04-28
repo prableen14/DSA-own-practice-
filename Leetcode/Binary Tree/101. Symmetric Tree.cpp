6d1b03b9f3ae388dc44a1fdd6f2e9fe1603483a4
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-04-28 23:58:58 +0530

    Update 101. Symmetric Tree.cpp

--- Leetcode/Binary Tree/101. Symmetric Tree.cpp
+++ Leetcode/Binary Tree/101. Symmetric Tree.cpp
@@ -31,7 +31,7 @@ public:
     bool isMirror(TreeNode* t1, TreeNode* t2){
         if(t1==NULL && t2==NULL) return true;
         if(t1==NULL || t2==NULL) return false;
-        
+      
         return (t1->val==t2->val) && isMirror(t1->left,t2->right)&& isMirror(t1->right,t2->left);
     }
 };
