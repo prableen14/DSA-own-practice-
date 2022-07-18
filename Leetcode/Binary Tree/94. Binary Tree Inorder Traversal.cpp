a0ae83ee53aa6ed91b7808837e04dae3d35af24b
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-07-19 00:53:18 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- Leetcode/Binary Tree/94. Binary Tree Inorder Traversal.cpp
+++ Leetcode/Binary Tree/94. Binary Tree Inorder Traversal.cpp
@@ -19,3 +19,18 @@ The number of nodes in the tree is in the range [0, 100].
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        inorder(root,nodes);
+        return nodes;
+    }
+    void inorder(TreeNode* root, vector<int>&nodes){
+        if(root==NULL) return;
+        
+        inorder(root->left,nodes);
+        nodes.push_back(root->val);
+        inorder(root->right,nodes);
+    }
+};
