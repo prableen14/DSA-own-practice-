35ebdc3600e149071c1f4672b273ea3a7268d589
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-07-29 00:09:11 +0530

    Update 100. Same Tree.cpp

--- Leetcode/Binary Tree/100. Same Tree.cpp
+++ Leetcode/Binary Tree/100. Same Tree.cpp
@@ -28,3 +28,25 @@ public:
         
     }
 };
+//ANOTHER WAY
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+         
+    // If both are NULL trees, they are identical.       
+    if(root1 == NULL && root2 == NULL){
+        return true;
+    }
+    
+    // If any one of the trees is NULL, they are not identical. 
+    if(root1 == NULL || root2 == NULL){
+        return false;
+    }
+    
+    // Check if root's data of both the trees is same. 
+    // Recursively check if left subtree and right subtree of both trees is identical. 
+    // Return true only if all conditions satisfy. 
+    
+    return (root1->data == root2->data) && 
+            identicalTrees(root1->left, root2->left) && 
+            identicalTrees(root1->right, root2->right);
+    
+}
