d8dfd137883eb32ac421e463f59fc45b67e86831
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-08-03 23:31:03 +0530

    Create Bottom View Of A Binarytree.cpp

---
+++ Level Up/Trees/Bottom View Of A Binarytree.cpp
@@ -0,0 +1,30 @@
+/*
+1. Given a Binary Tree, print Bottom View of it.
+Input Format
+Input is managed for you.
+Output Format
+Output is managed for you.
+
+Constraints:
+0 <= Number of Nodes <= 10^5
+-1000 <= value of Node data <= 1000
+Sample Input
+15
+1
+1
+-1
+1
+1
+-1
+1
+-1
+-1
+1
+-1
+-1
+1
+-1
+-1
+Sample Output
+1 1 1 
+*/
