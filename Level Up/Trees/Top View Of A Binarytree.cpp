294007f3af7c3a699394b0592eb8cf66d142a341
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-08-03 23:32:01 +0530

    Create Top View Of A Binarytree.cpp

---
+++ Level Up/Trees/Top View Of A Binarytree.cpp
@@ -0,0 +1,30 @@
+/*
+1. Given a Binary Tree, print top View of it. 
+2. For more Information watch given video link below.
+Input Format
+Input is managed for you.
+Output Format
+Output is managed for you. 
+Constraints
+0 <= Number of Nodes <= 10^5
+-1000 <= value of Node data <= 1000
+Sample Input
+15
+1
+1
+-1
+1
+1
+-1
+1
+-1
+-1
+1
+-1
+-1
+1
+-1
+-1
+Sample Output
+1 1 1
+*/
