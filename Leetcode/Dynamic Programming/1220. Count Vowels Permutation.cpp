799eaa4e17747b0a926c8e0385663142370c76a7
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-08-07 22:40:13 +0530

    Update 1220. Count Vowels Permutation.cpp

--- Leetcode/Dynamic Programming/1220. Count Vowels Permutation.cpp
+++ Leetcode/Dynamic Programming/1220. Count Vowels Permutation.cpp
@@ -25,3 +25,44 @@ Output: 68
 Constraints:
 1 <= n <= 2 * 10^4
 */
+//BRUTE FORCE
+class Solution {
+      const int MOD = 1e9 + 7;
+    const unordered_map<char, vector<char>> mappings{ {'s', {'a', 'e', 'i', 'o', 'u'} }, // start
+                                                      {'a', {'e'}                     }, 
+                                                      {'e', {'a', 'i'}                }, 
+                                                      {'i', {'a', 'e', 'o', 'u'}      }, 
+                                                      {'o', {'i', 'u'}                },
+                                                      {'u', {'a'}                     }  };
+    public:
+        int countVowelPermutation(int n) {
+        return solve(n, 's');                         // start with s
+    }
+    int solve(int rem, char prev) {
+        if(rem == 0) return 1;                        // no need to pick further. We have formed 1 string of length = n.
+        int ans = 0;
+        for(auto c : mappings.at(prev))               // try each vowel allowed after prev character
+            ans = (ans + solve(rem - 1, c)) % MOD;  
+        return ans;
+    }
+};
+//OPTIMISED
+//THIS QUESTION AND KNIGHT DIALER QUESTION (935) IS ALMOST SAME
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+         long a = 1, e = 1, i = 1, o = 1, u = 1, mod = pow(10, 9)+7;
+        long a2, e2, i2, o2, u2; 
+        
+        for (int j = 2; j <= n; j++) {
+            a2 = (e + i + u) % mod;
+            e2 = (a + i) % mod;
+            i2 = (e + o) % mod;
+            o2 = i;
+            u2 = (o + i) % mod;
+            
+            a = a2, e = e2, i = i2, o = o2, u = u2;
+        }
+        return (a + e + i + o + u) % mod;
+    }
+};
