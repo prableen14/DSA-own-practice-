4bad32204c4bbf8f28c012b08ae6ed0fda9d65e8
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-08-12 23:37:10 +0530

    Update All Possible Full Binary Trees.cpp

--- Level Up/Trees/All Possible Full Binary Trees.cpp
+++ Level Up/Trees/All Possible Full Binary Trees.cpp
@@ -1,5 +1,4 @@
 /*
-Easy  Prev   Next
 Given an integer n, return a list of all possible full binary trees with n nodes. Each node of each tree in the answer must have Node.val == 0.
 
 Each element of the answer is the root node of one possible tree. You may return the final list of trees in any order.
