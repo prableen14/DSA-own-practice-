7212c1b21c65b09fc248704b8a11e9b93b60f694
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-08-18 15:27:25 +0530

    Create 1338. Reduce Array Size to The Half.cpp

---
+++ Leetcode/Arrays/1338. Reduce Array Size to The Half.cpp
@@ -0,0 +1,41 @@
+/*
+You are given an integer array arr. You can choose a set of integers and remove all the occurrences of these integers in the array.
+Return the minimum size of the set so that at least half of the integers of the array are removed.
+
+Example 1:
+Input: arr = [3,3,3,3,5,5,5,2,2,7]
+Output: 2
+Explanation: Choosing {3,7} will make the new array [5,5,5,2,2] which has size 5 (i.e equal to half of the size of the old array).
+Possible sets of size 2 are {3,5},{3,2},{5,2}.
+Choosing set {2,7} is not possible as it will make the new array [3,3,3,3,5,5,5] which has a size greater than half of the size of the old array.
+
+Example 2:
+Input: arr = [7,7,7,7,7,7]
+Output: 1
+Explanation: The only possible set you can choose is {7}. This will make the new array empty.
+
+Constraints:
+
+2 <= arr.length <= 105
+arr.length is even.
+1 <= arr[i] <= 105
+*/
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int, int> counter;
+        priority_queue<int> q;
+        int res = 0, removed = 0;
+        
+        for (auto a : arr) counter[a]++;
+        for (auto c : counter) q.push(c.second);
+        
+        while (removed < arr.size() / 2) {
+            removed += q.top();
+            q.pop();
+            res++;
+        }
+        
+        return res;
+    }
+};
