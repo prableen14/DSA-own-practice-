0898596c7ad98a8f7be8480de8bff25d43233124
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-08-30 22:26:51 +0530

    Update 48. Rotate Image.cpp

--- Leetcode/Arrays/48. Rotate Image.cpp
+++ Leetcode/Arrays/48. Rotate Image.cpp
@@ -15,3 +15,21 @@ n == matrix.length == matrix[i].length
 1 <= n <= 20
 -1000 <= matrix[i][j] <= 1000
 */
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+          int n = matrix.size();
+        
+        //first we take transpose of the matrix
+        for(int i = 0; i < n; ++i){
+            for(int j = i; j < n; ++j){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        //then we take mirror image about the middle vertical line
+        for(int i = 0; i < n; ++i)
+            reverse(matrix[i].begin(), matrix[i].end());
+            
+    }
+};
