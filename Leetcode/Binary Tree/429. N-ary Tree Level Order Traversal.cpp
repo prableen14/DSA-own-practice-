fe94cb304f8541b18723d21e090c26ad5964fcec
Prableen Kaur Sachdeva <67010590+prableen14@users.noreply.github.com>
2022-09-05 21:44:47 +0530

    Update 429. N-ary Tree Level Order Traversal.cpp

--- Leetcode/Binary Tree/429. N-ary Tree Level Order Traversal.cpp
+++ Leetcode/Binary Tree/429. N-ary Tree Level Order Traversal.cpp
@@ -56,3 +56,20 @@ public:
         return ans;
     }
 };
+
+//DFS Solution
+class Solution { 
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> levelOrder(Node* root) {
+        dfs(root, 0);
+        return ans;
+    }
+    void dfs(Node* root, int level) {
+        if (root == nullptr) return;
+        if (level == ans.size()) ans.emplace_back(); // Need more level -> Add new level , push_back can also be used. See GFG for difference between emplace and push
+        ans[level].push_back(root->val);
+        for (Node* child : root->children)
+            dfs(child, level + 1);
+    }
+};
